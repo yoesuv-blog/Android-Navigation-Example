dd7717b5db088bf537932ce69cb0c067a3302c54
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-06-25 14:03:06 +0700

    :newspaper: add first fragment

---
+++ app/src/main/java/com/yoesuv/mynavigation/FirstFragment.kt
@@ -0,0 +1,14 @@
+package com.yoesuv.mynavigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FirstFragment  : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = LayoutInflater.from(context).inflate(R.layout.first_fragment, container, false)
+        return view
+    }
+}

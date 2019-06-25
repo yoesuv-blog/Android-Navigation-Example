08f9daf5e268d6ee4a5b6d4b80160c89a7b0c6a3
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-06-25 16:37:15 +0700

    :hammer: refactoring code

--- app/src/main/java/com/yoesuv/mynavigation/FirstFragment.kt
+++ app/src/main/java/com/yoesuv/mynavigation/FirstFragment.kt
@@ -10,8 +10,7 @@ import kotlinx.android.synthetic.main.first_fragment.view.*
 
 class FirstFragment  : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.first_fragment, container, false)
-        return view
+        return inflater.inflate(R.layout.first_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/yoesuv/mynavigation/SecondFragment.kt
+++ app/src/main/java/com/yoesuv/mynavigation/SecondFragment.kt
@@ -5,10 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.second_fragment.view.*
 
 class SecondFragment: Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.second_fragment, container, false)
-        return view
+        return inflater.inflate(R.layout.second_fragment, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.buttonSecondFragment.setOnClickListener {
+
+        }
+    }
+
 }

88264e83aa54ebea53aed0e56197ecaa0e188cce
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-06-25 16:19:00 +0700

    :sparkles: setup toolbar

--- app/src/main/java/com/yoesuv/mynavigation/MainActivity.kt
+++ app/src/main/java/com/yoesuv/mynavigation/MainActivity.kt
@@ -2,11 +2,27 @@ package com.yoesuv.mynavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbarMain)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navController = Navigation.findNavController(this, R.id.fragmentMain)
+        setupActionBarWithNavController( navController)
+        NavigationUI.setupWithNavController(toolbarMain, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.fragmentMain).navigateUp()
     }
 }

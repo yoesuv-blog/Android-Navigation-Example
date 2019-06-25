8260acefbec0ee72af7c22a74d0f34f8b44c742d
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-06-25 14:52:36 +0700

    :sparkles: success navigate to second fragment

--- app/src/main/java/com/yoesuv/mynavigation/FirstFragment.kt
+++ app/src/main/java/com/yoesuv/mynavigation/FirstFragment.kt
@@ -5,10 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.first_fragment.view.*
 
 class FirstFragment  : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.first_fragment, container, false)
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.buttonFirstFragment.setOnClickListener {
+            it.findNavController().navigate(R.id.action_to_fragment_second)
+        }
     }
 }
